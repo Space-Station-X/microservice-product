176e8ef0f9197050d03bec1f95b59b98ce93b18b
alejandro321ADR <alejmatam19.yui@hotmail.com>
2025-02-13 17:55:12 -0500

    agrege variables entorno

--- src/main/java/com/practice/controller/VideojuegosController.java
+++ src/main/java/com/practice/controller/VideojuegosController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.practice.entity.VideojuegosEntity;
 import com.practice.service.VideojuegosService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/videojuegos")
 public class VideojuegosController {
 
