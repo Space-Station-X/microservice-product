6ff8c80b81af121b418a99adef25fe99b949454c
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-02-21 08:25:17 -0500

    Add changes for swagger and module exceptions

--- src/main/java/com/practice/controller/VideojuegosController.java
+++ src/main/java/com/practice/controller/VideojuegosController.java
@@ -2,6 +2,9 @@ package com.practice.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,49 +13,56 @@ import org.springframework.web.bind.annotation.RestController;
 import com.practice.entity.VideojuegosEntity;
 import com.practice.service.VideojuegosService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/videojuegos")
+@Tag(name = "VideoJuegos", description = "VideoJuegos  API")
 public class VideojuegosController {
 
     @Autowired
     private VideojuegosService videojuegosService;
 
+    @Operation(summary = "Obtener todo los VideoJuegos", description = "Devuelve todo los VideoJuegos")
+    @ApiResponse(responseCode = "200", description = "VideoJuegos obtenidos correctamente")
+    @ApiResponse(responseCode = "404", description = "No se encontraron VideoJuegos")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VideojuegosEntity> listarVideojuegos() {
         return videojuegosService.getAllVideojuegos();
     }
 
+    @Operation(summary = "Obtener  videojuego por ID", description = "Devuelve videojuego por ID")
+    @ApiResponse(responseCode = "200", description = "Cliente obtenidos correctamente")
+    @ApiResponse(responseCode = "404", description = "No se encontró el videojuego")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public VideojuegosEntity obtenerVideojuego(@PathVariable("id") Integer id) {
         return videojuegosService.getVideojuegoById(id);
     }
 
+    @Operation(summary = "Crear un videojuego", description = "Crea  un  videojuego")
+    @ApiResponse(responseCode = "200", description = "Cliente creado correctamente")
+    @ApiResponse(responseCode = "404", description = "No se pudo crear el videojuego")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public VideojuegosEntity guardarVideojuego(@RequestBody VideojuegosEntity videojuego) {
         return videojuegosService.saveVideojuego(videojuego);
     }
-    
+
+    @Operation(summary = "Actualiza un videojuego", description = "Actualiza  un videojuego por ID")
+    @ApiResponse(responseCode = "200", description = "Cliente actualizado correctamente")
+    @ApiResponse(responseCode = "404", description = "No se pudo actualizar el videojuego")
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public VideojuegosEntity actualizarVideojuego(@PathVariable Integer id, @RequestBody VideojuegosEntity videojuego) {
         videojuego.setId(id);
         return videojuegosService.updateVideojuego(videojuego);
     }
 
+    @Operation(summary = "Elimina un videojuego por ID", description = "Elimina un videojuego por ID")
+    @ApiResponse(responseCode = "200", description = "Cliente eliminado correctamente")
+    @ApiResponse(responseCode = "404", description = "No se encontró el videojuego")
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public void eliminarVideojuego(@PathVariable Integer id) {
         videojuegosService.deleteVideojuego(id);
-        }
+    }
 
 }
